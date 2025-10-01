563570a6d37c69026d658cf6f9bc0504dcc55339
James Floyd <Commanderpip@gmail.com>
2025-09-30 22:22:57 -0400

    Relax

---
+++ lumine/src/main/java/com/paimon/lumine/Retraining.java
@@ -0,0 +1,14 @@
+package com.paimon.lumine;
+
+public class Retraining { 
+    public static void main(String[] args) {
+        int[] n = {1,2,3,4,5};
+        System.out.println(n.length);
+        if(n.includes(6)){
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }   
+
+}
