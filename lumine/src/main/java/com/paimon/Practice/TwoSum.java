04fbdd34626e274048b231bdd6a4a9f58ac6ee43
James Floyd <Commanderpip@gmail.com>
2026-05-11 22:08:18 -0400

    space remove

--- lumine/src/main/java/com/paimon/Practice/TwoSum.java
+++ lumine/src/main/java/com/paimon/Practice/TwoSum.java
@@ -13,8 +13,7 @@ public class TwoSum {
 
     public static int[] twoSum(int[] nums, int target){
         HashMap<Integer, Integer> map = new HashMap<>();
-
-
+        
         for(int i = 0; i <nums.length; i++){
             int complement = target - nums[i];
             if(map.containsKey(complement)){
@@ -22,13 +21,7 @@ public class TwoSum {
             }
             map.put(nums[i], i);
         }
-
-
-
-
-
         return new int[]{-1, -1}; // Placeholder return value
-
     }
 
 
